929e539bce7fe2b5cc4f02affa4b9addbe067d8b
Mariam <117775223+Jolinejo@users.noreply.github.com>
2024-07-05 04:19:54 +0000

    trying smth

--- working2/compile.sh
+++ working2/compile.sh
@@ -5,6 +5,6 @@ nasm -f bin bootloader.asm -o bin/bootloader.bin
 
 # compile program and link it
 nasm centry.asm -f elf64 -o bin/centry.o 
-gcc -ffreestanding -c program.c -o bin/program.o -march=x86-64
-ld -o bin/program.bin -T link.ld --oformat binary bin/centry.o bin/program.o
-cat bin/bootloader.bin bin/program.bin > bin/image
+gcc -ffreestanding -c kernel.c -o bin/kernel.o -march=x86-64
+ld -o bin/kernel.bin -T link.ld --oformat binary bin/centry.o bin/kernel.o
+cat bin/bootloader.bin bin/kernel.bin > bin/image
